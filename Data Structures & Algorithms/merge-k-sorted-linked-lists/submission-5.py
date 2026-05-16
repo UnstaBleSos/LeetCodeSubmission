0af1874e50eaf89ffb437786048cc76849ff425a
Ashutosh Khanal <142417290+UnstaBleSos@users.noreply.github.com>
2026-05-16 09:59:56 +0545

    Add: merge-k-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-5.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: 
+            return None
+        
+        while len(lists)>1:
+            sortedLists = []
+            for i in range(0,len(lists)-1,2):
+                head = ListNode()
+                tail = head
+                while lists[i] and lists[i+1]:
+                    if lists[i].val < lists[i+1].val:
+                        tail.next = lists[i]
+                        lists[i] = lists[i].next
+                    else:
+                        tail.next = lists[i+1]
+                        lists[i+1] = lists[i+1].next
+                    tail = tail.next
+                
+                while lists[i]:
+                    tail.next = lists[i]
+                    lists[i] = lists[i].next
+                    tail = tail.next
+
+                while lists[i+1]:
+                    tail.next = lists[i+1]
+                    lists[i+1] = lists[i+1].next 
+                    tail = tail.next
+                
+                sortedLists.append(head.next)
+            if len(lists) %2 != 0:
+                sortedLists.append(lists[-1])
+            
+            lists= sortedLists
+        
+        return lists[0]           
