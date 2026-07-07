e05b88811f852205f9db13ef7dd5e47b07ea12d9
Ashutosh Khanal <142417290+UnstaBleSos@users.noreply.github.com>
2026-07-07 22:42:06 +0545

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,57 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        output = set()
+        path = set()
+        for ch in words:
+            curr= root
+            for chars in ch:
+                if chars not in curr.children:
+                    curr.children[chars] = TrieNode()
+                curr = curr.children[chars]
+            curr.isWord = True
+        row,col = len(board), len(board[0])
+        pointer = root
+        def backTrack(r,c,pointer,word):
+            if not pointer.children:
+                return 
+                
+            if r >= row or c >= col:
+                return False
+            
+            if r< 0 or c< 0:
+                return False
+            
+            if (r,c) in path:
+                return
+            
+            if board[r][c] not in pointer.children:
+                return False 
+            else:
+                pointer = pointer.children[board[r][c]]
+
+            path.add((r,c))
+            word  += board[r][c]
+            if pointer.isWord == True:
+                output.add(word)
+            backTrack(r+1,c,pointer,word)
+            backTrack(r-1,c, pointer, word)
+            backTrack(r,c+1,pointer,word)
+            backTrack(r,c-1, pointer, word)
+
+            path.remove((r,c))
+
+            
+        
+        for i in range(row):
+            for j in range(col):
+                backTrack(i,j,root,"")
+
+        return list(output)
+            
+            
