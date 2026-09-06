19f7114212fca420b7adf4b5513ffb81914a7271
Ashutosh Khanal <142417290+UnstaBleSos@users.noreply.github.com>
2026-09-06 20:59:53 +0545

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount+1)
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(amount+1):
+                remaining = i - coin
+                if remaining < 0 :
+                    continue
+
+                if i < 0 :
+                    continue
+                
+                dp[i] +=  dp[remaining]
+        
+
+        return dp[amount]
